cd4f188bf11451e75c5dc288774c6a48b38fa47b
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-02-17 12:45:37 -0800

    Scaffolding set up with `yo tool`
    TODO: make it actually create some files

---
+++ generator-tool/app/index.js
@@ -0,0 +1,14 @@
+var generators = require('yeoman-generator');
+
+module.exports = generators.Base.extend({
+  constructor: function () {
+    generators.Base.apply(this, arguments);
+    this.option('coffee');
+  },
+  method1: function() {
+    console.log('method 1 just ran');
+  },
+  method2: function() {
+    console.log('method 2 just ran');
+  },
+});
