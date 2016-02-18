355a90bf1863f8bbc03a75da40b91656919390df
Michael Hinrichs <michaelghinrichs@gmail.com>
2016-02-18 06:32:15 -0800

    Remove spaces and capitalize

--- generator-tool/app/index.js
+++ generator-tool/app/index.js
@@ -4,6 +4,10 @@ var mkdirp = require('mkdirp');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
+function capitalize(str) {
+  return str.replace(/(?:^|\s)\S/g, function(a) { return a.toUpperCase(); });
+};
+
 module.exports = generators.Base.extend({
   constructor: function () {
     generators.Base.apply(this, arguments);
@@ -22,24 +26,25 @@ module.exports = generators.Base.extend({
     }];
 
     this.prompt(prompts, function(answers) {
-      this.toolName = answers.toolName;
+      this.toolName = capitalize(answers.toolName).replace(/\s+/g, '');
 
+      this.toolBase = 'src/_shared/tools/' + this.toolName;
       done();
     }.bind(this));
   },
 
-  app: function () {
-    mkdirp('src/_shared/tools/' + this.toolName + '/components');
-    mkdirp('src/_shared/tools/' + this.toolName + '/redux/ducks');
-    mkdirp('src/_shared/tools/' + this.toolName + '/redux/selectors');
-    mkdirp('src/_shared/tools/' + this.toolName + '/redux/dispatch');
+  dirs: function () {
+    mkdirp(this.toolBase + '/components');
+    mkdirp(this.toolBase + '/redux/ducks');
+    mkdirp(this.toolBase + '/redux/selectors');
+    mkdirp(this.toolBase + '/redux/dispatch');
   },
 
   writing: function () {
     this.fs.copyTpl(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt'),
-      { title: this.toolName }
+      this.templatePath('index.js'),
+      this.destinationPath(this.toolBase + '/components/index.js'),
+      { tool: this.toolName }
     );
   },
 });
